888e9afffc484c7f86e0b74f443e2782bf5f2d51
Karina-Ba <batmanikarina@gmail.com>
2022-02-05 00:27:38 +0200

    About & more posts logic

--- site/js/Socials.js
+++ site/js/Socials.js
@@ -63,6 +63,73 @@ class NewPost extends React.Component {
     }
 }
 
+class LatestPost extends React.Component {
+    constructor(props) {
+		super(props);
+	}
+
+    render() {
+        return React.createElement(
+            "div",
+            { className: "post-container", id: "write-post" },
+            React.createElement(
+                "form",
+                { className: "form", onSubmit: this.handle_post },
+            React.createElement(
+                "div",
+                { className: "post-text" },
+                "Write a post:"
+                ),
+                React.createElement(
+                    "div",
+                    { className: "flex-box" },
+                    React.createElement(
+                        "div",
+                        { className: "input-group" },
+                        React.createElement("input",
+                        { className: "post-input", type: "text", name: "post", placeholder: "I'm feeling..." }
+                        ),
+                    ),
+                    React.createElement(
+                        "button",
+                        { className: "secondary" },
+                        "Post"
+                    ),	
+                ),
+              ),
+        );
+    }
+}
+
+class Profile extends React.Component {
+    constructor(props) {
+		super(props);
+	}
+
+    render() {
+        return React.createElement(
+            "div",
+            { className: "profile-container", id: "profile" },
+            React.createElement(
+            "div",
+            { className: "profile-pic" + this.props.profile_pic, id: "profile-pic" }
+            ), 
+            React.createElement(
+                "div",
+                { className: "homepage-text", id: "user-info" },
+                "User ID: ",
+                React.createElement("br", null),
+                this.props.u_id,
+                React.createElement("br", null),
+                React.createElement("br", null),
+                "Full name: ",
+                React.createElement("br", null),
+                this.props.full_name,
+            ),
+        );
+    }
+}
+
 class Message extends React.Component {
     constructor(props) {
 		super(props);
