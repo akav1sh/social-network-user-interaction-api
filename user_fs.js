5cfb3213907ad8372e80249ff067e2ed54b3c058
akav1sh <57904970+akav1sh@users.noreply.github.com>
2022-02-12 16:26:50 +0200

    fix messages

--- user_fs.js
+++ user_fs.js
@@ -154,7 +154,12 @@ async function get_messages(requesting_user_id, sender_id, status)
                 messages = messages.filter(message => message.m_status === status)
                 messages.forEach(message => message.m_status = "read")
             await fs.writeFile(db_file, JSON.stringify(db_json))
-            return messages
+            return messages.sort((p1, p2) => {
+                if (parseInt(p1.p_id) > parseInt(p2.p_id))
+                    return 1
+                else
+                    return -1
+            })
         })
 }
 
