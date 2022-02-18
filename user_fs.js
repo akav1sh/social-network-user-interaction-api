74d276f13ece5caa37c10bb17661162133ae3f91
akav1sh <57904970+akav1sh@users.noreply.github.com>
2022-02-18 16:24:28 +0200

    fix messages not to get self messages.

--- user_fs.js
+++ user_fs.js
@@ -150,6 +150,7 @@ async function get_messages(requesting_user_id, sender_id, status)
             // filter sender id to get messages from single user or all if id === "0"
             let messages = requesting_user.messages
                 .filter(message => message.sender_id === (sender_id !== "0" ? sender_id : message.sender_id))
+                .filter(message => message.sender_id !== requesting_user_id)
             if (status !== "all")
                 messages = messages.filter(message => message.m_status === status)
                 messages.forEach(message => message.m_status = "read")
